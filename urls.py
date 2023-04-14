8d67cc2117c2ffb01e10a77d8dd9ce9a93ccc0a7
Jaeden Cruz <jaedencruz4@gmail.com>
2023-04-14 07:45:28 -0400

    final project completion.

--- urls.py
+++ urls.py
@@ -15,7 +15,23 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.contrib.auth import views as auth_views
+from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path(''. views.HomeView.as_view(), name='home'),
+    path('ingredients/', views.IngredientList.as_view(), name='ingredients'),
+    path('ingredients/new', views.CreateIngredientView.as_view(), name='ingredient_create'),
+    path('ingredients/<int:pk>/update', views.UpdateIngredientView.as_view(), name='ingredient_update'),
+    path('ingredients/<int:pk>.delete', views.DeleteIngredientView.as_view(), name='ingredient_delete'),
+    path('menu/', views.MenuView.as_view(), name='menu'),
+    path('menu/new', views.NewMenuItem.as_view(), name='create_menu_item'),
+    path('menu/<int:pk>/update', views.UpdateMenuItem.as_view(), name='menu_update'),
+    path('menu/<int:pk>/delete', views.DeleteMenuItem.as_view(), name='menu_delete'),
+    path('reciperequirement/', views.RecipeRequirementView.as_view(), name='reciperequirement'),
+    path('reciperequirement/new', views.NewRecipeRequirement.as_view(), name='new_recipe_requirement'),
+    path('purchases/', views.PurchasesView.as_view(), name='purchases'),
+    path('purchases/new', views.NewPurchase.as_view(), name='add_purchase'),
+    path('reports', views.ReportView.as_view(), name='reports'),
 ]
